a6fd9e2af88f72494abb0a3e4d5d9902f94e7b10
butan-2-ol <champamartey01@gmail.com>
2025-11-07 19:06:05 +0000

    Initial commit - added Python projects

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+
+#A python script that performs basic arithmetic operations with two predefined numbers
+
+number1 = 17
+number2 = 23
+
+addition_result = number1 + number2
+subtraction_result = number2 - number1
+multiplication_result = number1 * number2
+division_result = number1 / number2
+
+print(f"Addition of {number1}   and {number2} is {addition_result}")
+print(f"Subtraction of {number1}   and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1}   and {number2} is {multiplication_result}")
+print(f"Division of {number1}   and {number2} is {division_result}")
